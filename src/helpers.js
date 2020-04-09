7fc3487803bc861b29c421572665bb88630549d9
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2020-04-09 14:33:45 +0100

    improved helper functions

--- src/helpers.js
+++ src/helpers.js
@@ -3,13 +3,13 @@ const currentlyInfected = (cases, multiples) => cases * multiples;
 
 const infectionsByRequestedTime = (infected, duration) => infected * duration;
 
-const severeCasesByRequestedTime = (infectionsByTime) => infectionsByTime * 0.15;
+const severeCasesByRequestedTime = (infectionsByTime) => Math.trunc(infectionsByTime * 0.15);
 
 const hospitalBedsByRequestedTime = (totalBeds, severeCases) => Math.trunc((totalBeds * 0.35) - severeCases);
 
-const casesForICUByRequestedTime = (infectionsByTime) => infectionsByTime * 0.05;
+const casesForICUByRequestedTime = (infectionsByTime) => Math.trunc(infectionsByTime * 0.05);
 
-const casesForVentilatorsByRequestedTime = (infectionsByTime) => infectionsByTime * 0.02;
+const casesForVentilatorsByRequestedTime = (infectionsByTime) => Math.trunc(infectionsByTime * 0.02);
 
 const dollarsInFlight = (infectionsByTime, income, population, duration) => infectionsByTime * income * population * duration;
 
